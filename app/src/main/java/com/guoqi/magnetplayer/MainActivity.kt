ba96b11ba528cf3e2760a02fff13377f6c171a22
guoqi <1395772260@qq.com>
2018-09-13 09:00:09 +0800

    增加解析种子

--- app/src/main/java/com/guoqi/magnetplayer/MainActivity.kt
+++ app/src/main/java/com/guoqi/magnetplayer/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.guoqi.magnetplayer
 
+import android.Manifest
 import android.app.ProgressDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.Settings
 import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.alibaba.fastjson.JSON
+import com.guoqi.magnetplayer.ttorrent.common.Torrent
 import com.lzy.okgo.OkGo
 import com.lzy.okgo.callback.StringCallback
 import com.lzy.okgo.model.Response
@@ -17,13 +28,14 @@ import com.scwang.smartrefresh.layout.header.ClassicsHeader
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
+import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         //source=种子搜&keyword=测试&page=1
-        val filePath = Environment.getExternalStorageDirectory().absolutePath + File.separator + "MagnetPlayer"
+        val rootPath = Environment.getExternalStorageDirectory().absolutePath + File.separator + "MagnetPlayer"
         const val LOAD_REFRESH = 1001
         const val LOAD_MORE = 1002
     }
@@ -44,8 +56,13 @@ class MainActivity : AppCompatActivity() {
         initData()
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1);
+            } else {
+                decodeTorrent()
+
+            }
         }
     }
 
@@ -134,4 +151,63 @@ class MainActivity : AppCompatActivity() {
                 })
 
     }
+
+    private fun isMagnet(magnet: String): Boolean {
+        return Pattern.matches("^(magnet:\\?xt=urn:btih:)[0-9a-fA-F]{40}.*$", magnet)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                decodeTorrent()
+            } else {
+                showNoPermissionDialog(this, "文件读写")
+            }
+        }
+    }
+
+    private fun showNoPermissionDialog(context: Context, str: String) {
+        AlertDialog.Builder(context).setTitle("获取" + str + "权限被禁用")
+                .setMessage("请在 设置-应用管理-" + context.getString(R.string.app_name) + "-权限管理 (将" + str + "权限打开)")
+                .setNegativeButton("取消", null)
+                .setPositiveButton("去设置", DialogInterface.OnClickListener { _, _ ->
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    intent.data = Uri.parse("package:" + context.packageName)
+                    context.startActivity(intent)
+                }).show()
+    }
+
+    private fun decodeTorrent() {
+        Snackbar.make(toolbar, "正在解析种子", Snackbar.LENGTH_LONG).show()
+//            val address = InetAddress.getLocalHost()
+//            var newFile = File(Environment.getExternalStorageDirectory().absolutePath + "/newFile")
+//            Util.writeBytesToFile(assets.open("test.torrent"), newFile)
+//            val torrent = SharedTorrent.fromFile(newFile, File(Environment.getExternalStorageDirectory().absolutePath))
+//            val client = Client(address, torrent)
+//            client.download()
+        var `is` = assets.open("test.torrent")
+        var file = File(rootPath + "/bt.torrent")
+//        if (!file.exists()) {
+//            file.mkdirs()
+//        }
+        Util.writeBytesToFile(`is`, file)
+        var torrent = Torrent.load(file)
+        Log.e("torrent: name:", torrent.name)
+        Log.e("torrent: comment:", torrent.comment)
+        Log.e("torrent: createdBy:", torrent.createdBy)
+        Log.e("torrent: hexInfoHash:", torrent.hexInfoHash)
+        Log.e("torrent: announceList:", torrent.announceList.toString())
+        Log.e("torrent: size:", torrent.size.toString())
+        Log.e("torrent: isSeeder:", torrent.isSeeder.toString())
+        Log.e("torrent: trackerCount:", torrent.trackerCount.toString())
+
+//        val tracker = Tracker(6969)
+//        val filter = FilenameFilter { dir, name -> name.endsWith(".torrent") }
+//        for (f in File("/path/to/torrent/files").listFiles(filter)) {
+//            tracker.announce(TrackedTorrent.load(f))
+//        }
+//        tracker.setAcceptForeignTorrents(true)
+//        tracker.start(true);
+    }
+
 }
